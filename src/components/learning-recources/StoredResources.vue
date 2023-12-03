3aa71ac7ddbe90442b750be905f50bb9befdd8eb
SabaMuladze <muladze66@bk.ru>
2023-12-03 15:52:27 +0400

    progress

---
+++ src/components/learning-recources/StoredResources.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+    <learning-resource v-for="resource in resources" :key="resource.id" :link="resource.link"
+      :title="resource.title" :description="resource.description"></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default{
+    data() {
+        return {
+            
+        }
+    },
+    props:['resources'],
+    components:{
+        LearningResource
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
